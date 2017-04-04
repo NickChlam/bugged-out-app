fa545634608b49a34ecac612190908914e189faf
NickChlam <Nick.Chlam@rht.com>
2017-04-03 22:01:18 -0400

    added navbar to nabar.compnent.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<router-outlet> </router-outlet>'
+    template: `
+    <navbar></navbar>
+    <router-outlet> </router-outlet>
+    `
 })
  
 export class AppComponent { }
- 
+  
