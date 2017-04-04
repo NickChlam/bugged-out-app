617e65b974f2949111a2037a1ee7c33e74a8e490
NickChlam <Nick.Chlam@rht.com>
2017-04-04 08:09:27 -0400

    routing completed

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 
 
 @Component({
+    
     selector: 'my-app',
     template: `
     <navbar></navbar>
-    <router-outlet> </router-outlet>
-    `
+        <div class="container">
+            <router-outlet> </router-outlet>
+        </div>
+    `,
+    styles: [
+        `
+        .container {
+            margin-top: .5rem;
+        }
+        `
+    ]
 })
  
 export class AppComponent { }
--- app/not-found.component.ts
+++ app/not-found.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'not-found',
-    template: '<h1> Page not Found </h1>'
+    template: '<h1>404 Page not Found </h1>'
 
 })
 
